39edc12d4418977086fbc8cb376504474cf0da36
namkoun <70187334+namkoun@users.noreply.github.com>
2023-04-21 13:15:14 +0900

    [unrated] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -0,0 +1,12 @@
+function solution(my_string, alp) {
+    var answer = my_string.split("");
+    
+    for(let i =0;i<answer.length;i++){
+        if(answer[i] == alp){
+            answer[i] = answer[i].toUpperCase() 
+        }
+    }
+    
+    
+    return answer.join("");
+}
