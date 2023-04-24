de624fc62e80465e7064b4e88ffd1c6bb3d293d1
namkoun <70187334+namkoun@users.noreply.github.com>
2023-04-24 09:24:30 +0900

    [unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,18 @@
+function solution(n) {
+    var answer = 0;
+    if(n % 2 == 0){
+        for(let i = n; i>0;i--){
+            if(i%2==0){
+                answer += i**2
+            }
+        }
+    }else{
+         for(let i = n; i>0;i--){
+            if(i%2==0){
+            }else{
+                   answer += i
+            }
+        }
+    }
+    return answer;
+}
