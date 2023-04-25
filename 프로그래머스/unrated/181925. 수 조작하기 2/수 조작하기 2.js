031057b6002b3a65e614691e8f1e590c4260ba6a
namkoun <70187334+namkoun@users.noreply.github.com>
2023-04-25 13:20:49 +0900

    [unrated] Title: 수 조작하기 2, Time: 7.13 ms, Memory: 41.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,15 @@
+function solution(numLog) {
+    var answer = '';
+    for(let i = 1;i<numLog.length;i++){
+        if(numLog[i - 1] + 1 == numLog[i]){
+            answer += "w"
+        }else if(numLog[i - 1] - 1 == numLog[i]){
+            answer += "s"
+        }else if(numLog[i - 1] + 10 == numLog[i]){
+            answer += "d"
+        }else if(numLog[i - 1] - 10 == numLog[i]){
+            answer += "a"
+        } 
+    }
+    return answer;
+}
