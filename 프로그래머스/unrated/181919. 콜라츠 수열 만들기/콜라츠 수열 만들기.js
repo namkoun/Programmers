a4e7fe7cf65f780c6eecebccdbbef6712ca77e88
namkoun <70187334+namkoun@users.noreply.github.com>
2023-04-27 10:55:55 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    var answer = [n];
+    while(true){
+        if(n == 1){
+            break;
+        }
+        if(n%2==0){
+            n = n / 2
+            answer.push(n)
+        }else{
+            n = 3 * n + 1
+            answer.push(n)
+        }
+    }
+    return answer;
+}
